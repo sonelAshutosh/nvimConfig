9ae5e4a866f725a94cefd6129a5cd7683d589d73
sonelAshutosh <ashutoshhoonmain@gmail.com>
2025-01-03 22:37:31 +0530

    Added the indent, codium, neoscroll, and independent-blackline plugins

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
-vim.o.number = true              -- Show line numbers
-vim.o.relativenumber = true      -- Show relative line numbers
-vim.o.shiftwidth = 4            -- Number of spaces for indentation
-vim.o.expandtab = true          -- Use spaces instead of tabs
-vim.o.smartindent = true        -- Smart indentation
-vim.o.swapfile = false          -- Disable swap files
+-- Basic Settings (already present in your config)
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.swapfile = false
+vim.g.mapleader = " "
 
--- Clipboard (optional, uncomment to use system clipboard)
+-- Clipboard (optional)
 -- vim.o.clipboard = "unnamedplus"
 
 -- Load Lazy.nvim
@@ -16,56 +18,53 @@ if not vim.loop.fs_stat(lazypath) then
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Plugin Setup with Lazy.nvim
+-- Plugin Setup
 require("lazy").setup({
-    "folke/lazy.nvim",  -- Lazy.nvim plugin manager
+    "folke/lazy.nvim", -- Lazy.nvim plugin manager
+
+    -- Telescope for fuzzy searching
     {
-        "nvim-telescope/telescope.nvim",  -- Telescope for fuzzy searching
+        "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
+
+    -- Treesitter for syntax highlighting
     {
-        "nvim-treesitter/nvim-treesitter",  -- Treesitter for syntax highlighting
-        build = ":TSUpdate",  -- Updates and installs parsers
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { 
-                    "lua", 
-                    "python", 
-                    "javascript", 
-                    "html", 
-                    "css", 
-                    "tsx",  -- Includes React (JSX/TSX)
-                    "typescript", 
-                },
-                highlight = { enable = true },  -- Enable Treesitter syntax highlighting
+                ensure_installed = { "lua", "python", "javascript", "html", "css", "tsx", "typescript" },
+                highlight = { enable = true },
             })
         end,
     },
-    "neovim/nvim-lspconfig",  -- LSP support
+
+    -- LSP support
+    "neovim/nvim-lspconfig",
+
+    -- Autocompletion
     {
-        "hrsh7th/nvim-cmp",      -- Autocompletion plugin
+        "hrsh7th/nvim-cmp",
         dependencies = {
-            "hrsh7th/cmp-nvim-lsp",  -- LSP source for nvim-cmp
-            "hrsh7th/cmp-buffer",    -- Buffer source for nvim-cmp
-            "hrsh7th/cmp-path",      -- Path source for nvim-cmp
-            "saadparwaiz1/cmp_luasnip",  -- Snippet source for nvim-cmp
-            "L3MON4D3/LuaSnip",      -- Snippet engine
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
         },
         config = function()
             local cmp = require("cmp")
-            local lspconfig = require("lspconfig")
-
-            -- Setup nvim-cmp
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        require("luasnip").lsp_expand(args.body)  -- Use LuaSnip for expanding snippets
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 mapping = {
@@ -77,67 +76,116 @@ require("lazy").setup({
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
                 },
                 sources = {
-                    { name = "nvim_lsp" },  -- LSP completion
-                    { name = "buffer" },    -- Buffer completion
-                    { name = "path" },      -- Path completion
-                    { name = "luasnip" },   -- Snippet completion
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "luasnip" },
                 },
             })
-
-            -- Setup LSP (Example for Python)
-            lspconfig.pyright.setup{}  -- Use Pyright for Python LSP support
-            lspconfig.ts_ls.setup{} -- Use TSServer for JavaScript/TypeScript LSP support
-
         end,
     },
-    "nvim-lualine/lualine.nvim",  -- Status line plugin
-    "nvim-tree/nvim-tree.lua",    -- File explorer plugin
+
+    -- Null-LS for external tools
     {
-        "nvim-neo-tree/neo-tree.nvim",  -- Neo-tree plugin
-        branch = "v2.x",  -- Use the v2.x branch for the latest stable version
-        dependencies = {
-            "nvim-lua/plenary.nvim",  -- For necessary utility functions
-            "kyazdani42/nvim-web-devicons",  -- For file icons
-            "MunifTanjim/nui.nvim",    -- Dependency for Neo-tree UI
-        },
+        "jose-elias-alvarez/null-ls.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            require("neo-tree").setup({
-                filesystem = {
-                    filtered_items = {
-                        visible = true,  -- Show hidden files (adjust as needed)
-                    },
-                },
-                window = {
-                    position = "right",  -- Position the Neo-tree window to the left
-                    width = 30,         -- Set width of the Neo-tree window
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.diagnostics.eslint,
+                    null_ls.builtins.formatting.black,
                 },
             })
         end,
     },
-    { 
-        "catppuccin/nvim",  -- Catppuccin theme
-        name = "catppuccin",  -- Alias for convenience
-        priority = 1000,  -- Ensure it loads first
+
+    -- Inlay Hints
+    {
+        "simrat39/inlay-hints.nvim",
+        config = function()
+            require("inlay-hints").setup()
+        end,
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        version = "*",  -- Use the latest version
+        config = function()
+            require("ibl").setup({
+                indent = {
+                    char = "│",  -- Character for the indent guides
+                },
+                scope = {
+                    enabled = true,  -- Enable scope guides
+                    show_start = true,  -- Show the start of the scope
+                    show_end = false,  -- Hide the end of the scope
+                },
+            })
+        end,
+    },
+
+    -- Terminal Toggle
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup({
+                size = 20,
+                open_mapping = [[<C-t>]],
+                direction = "horizontal",
+            })
+        end,
+    },
+
+    -- AI-Assisted Code Completion
+    --{
+    --    "Exafunction/codeium.nvim",
+    --    config = function()
+    --        require("codeium").setup({})
+    --    end,
+    --},
+
+    -- Smooth Scrolling
+    {
+        "karb94/neoscroll.nvim",
+        config = function()
+            require("neoscroll").setup({
+                -- Customize scrolling speed and behavior here
+                easing_function = "quadratic", -- Default easing function
+                hide_cursor = false,           -- Keep the cursor visible while scrolling
+            })
+        end,
+    },
+
+    -- Catppuccin Theme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
     },
 })
 
--- Catppuccin Theme Configuration
-require("catppuccin").setup({
-    flavour = "mocha",  -- Choose theme flavour (latte, frappe, macchiato, mocha)
-})
-
--- Set colorscheme
+-- Colorscheme
+require("catppuccin").setup({ flavour = "mocha" })
 vim.cmd([[colorscheme catppuccin]])
 
--- Set leader key to space
-vim.g.mapleader = " "
+-- Ripgrep installation (Make sure it is installed on your system)
+-- You can install it using your package manager:
+-- For Debian/Ubuntu: sudo apt install ripgrep
+-- For macOS (with Homebrew): brew install ripgrep
 
--- Neo-tree keybindings
+-- Neo-tree File Explorer Keybinding
 vim.api.nvim_set_keymap("n", "<Leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
 
--- Keybindings for Telescope
-vim.api.nvim_set_keymap("n", "<Leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })  -- Search files
-vim.api.nvim_set_keymap("n", "<Leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })   -- Search text in files
-vim.api.nvim_set_keymap("n", "<Leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })     -- List open buffers
-vim.api.nvim_set_keymap("n", "<Leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })    -- Search help tags
+-- Telescope Keybindings
+vim.api.nvim_set_keymap("n", "<Leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })
 
+-- ToggleTerm Keybinding
+vim.api.nvim_set_keymap("n", "<C-t>", ":ToggleTerm<CR>", { noremap = true, silent = true })
+
+-- Inlay Hints Setup
+require("inlay-hints").setup()
